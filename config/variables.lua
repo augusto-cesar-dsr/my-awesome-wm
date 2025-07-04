6b60c3d855ba9a9b795ac50981e2d3ce2678e8e9
Augusto César <augusto_cesar.cc@hotmail.com>
2025-07-04 17:35:24 -0300

    feat: reorganizar tags e adicionar regras para Telegram/Discord
    - Tag 4: alterada para Terminal com ícone de console (󰆍)
    - Tag 5: alterada para Arquivos com ícone de pasta (󰉋)
    - Tag 6: configurada para Comunicação (Telegram/Discord)
    - Adicionadas regras automáticas para Telegram e Discord abrirem na tag 6
    - Nautilus agora abre automaticamente na tag 5
    - Atualizado README.md com nova organização das tags

--- config/variables.lua
+++ config/variables.lua
@@ -29,9 +29,9 @@ awful.screen.connect_for_each_screen(function(s)
     "󰨞", -- Editor
     "󰒱", -- Chat  
     "󰖟", -- Browser
+    "󰆍", -- Terminal
     "󰉋", -- Files
-    "󰝚", -- Music
-    "󰍹", -- Terminal
+    "󰍹", -- Communication
     "󰊻", -- Documents
     "󰟴", -- Settings
     "󰐃", -- Misc
